5469d3a9b7eeb9718d5ebf9bb866538e7552e0d7
Konstantin <nikst0r1963@gmail.com>
2023-05-23 22:40:37 +0300

    Add a stack for values and some basic binary operations

--- src/main.c
+++ src/main.c
@@ -10,10 +10,25 @@ int main(int argc, char *argv[]) {
 
   init_chunk(&chunk);
 
-  int constant = add_constant(&chunk, 2.1);
+  int constant = add_constant(&chunk, 2.2);
 
   write_chunk(&chunk, OP_CONSTANT, 420);
   write_chunk(&chunk, constant, 420);
+
+  constant = add_constant(&chunk, 3.4);
+
+  write_chunk(&chunk, OP_CONSTANT, 420);
+  write_chunk(&chunk, constant, 420);
+
+  write_chunk(&chunk, OP_ADD, 420);
+
+  constant = add_constant(&chunk, 5.6);
+
+  write_chunk(&chunk, OP_CONSTANT, 420);
+  write_chunk(&chunk, constant, 420);
+
+  write_chunk(&chunk, OP_DIVIDE, 420);
+  write_chunk(&chunk, OP_NEGATE, 420);
 
   write_chunk(&chunk, OP_RETURN, 420);
 
